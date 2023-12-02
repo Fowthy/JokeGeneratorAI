6876f05cbc231182acb869681125d9a504499a15
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-02 23:23:52 +0100

    Updated initial code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+st.title('🦜🔗 Joke Generator')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
+joke_type = st.sidebar.text_input('Joke Type')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+    if not input_text.endswith('?') and not input_text.endswith('.'):
+        st.warning('Please enter a complete sentence or question for better joke generation.', icon='⚠')
+        return
+
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    joke_prompt = f"Generate a {joke_type.lower()} joke: {input_text}"
+    joke_response = llm(joke_prompt)
+    st.info(joke_response)
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+    text = st.text_area('Enter keywords or topics for your joke.')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
