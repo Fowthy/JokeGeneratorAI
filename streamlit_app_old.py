eb7b6df5f7592819257194b0720326740d23000a
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-02 23:51:32 +0100

    Added PromtTemplate + ChatOpenAI model

--- streamlit_app_old.py
+++ streamlit_app_old.py
@@ -1,30 +1,16 @@
 import streamlit as st
-from langchain.llms import OpenAI, GPT3, DialoGPT
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 
-from langchain.schema import HumanMessage
-from langchain.prompts import PromptTemplate
-
-
-
-
-
-
-st.title('Task Planner')
+st.title('🦜🔗 Joke Generator')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 joke_type = st.sidebar.text_input('Joke Type')
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    chat_model = ChatOpenAI()
     joke_prompt = f"Generate a {joke_type.lower()} joke: {input_text}"
     joke_response = llm(joke_prompt)
-
-    messages = [HumanMessage(content=text)]
-    respomse = chat_model.invoke(messages)
-
-    st.info(respomse)
+    st.info(joke_response)
 
 
 
