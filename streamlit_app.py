17064eef2aae16804417729b329d840675622f33
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-02 23:58:27 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI, GPT3, DialoGPT
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 
