6269a02950edb4d115efc2ffd49e1029d62bafa3
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-03 00:38:15 +0100

    Added prompts to the chained models

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,36 +26,42 @@ def generate_response(input_text):
     criticism_model = ChatOpenAI(openai_api_key=openai_api_key)
     finalization_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    template = "You are an AI that generates jokes."
+    template = "You are an AI that generates jokes. You answer only what you were asked, in short, concrete answers."
+
+    template2 = f"{template} You analyse the joke and refine it in understandable way."
+
+    template3 = f"{template2} You critisize the joke and give feedback on what is funny and what is not funny. You output your answer with the joke first and then the feedback."
+
+    template4 = f"{template} You will get the joke, followed with a critique of the joke. You will then finalize the joke and output the final joke. Your goal is to make it as funny as possible."
 
     # Creative idea generation
     creative_prompt = ChatPromptTemplate.from_messages([
         ("system", template),
         ("human", f"Generate a creative joke about {input_text}")
     ])
-    creative_output = creative_model(creative_prompt.format_prompt(input_text=input_text).to_messages())  # Get creative output
+    creative_output = creative_model(creative_prompt.format_prompt(input_text=input_text).to_messages()) 
 
     # Language refinement
     refinement_prompt = ChatPromptTemplate.from_messages([
-        ("system", template),
-        ("human", f"Refine the language of the joke: {creative_output}")
+        ("system", template2),
+        ("human", f"Refine the the joke: {creative_output}")
     ])
-    refined_output = refinement_model(refinement_prompt.format_prompt(input_text=input_text).to_messages())  # Get refined output
+    refined_output = refinement_model(refinement_prompt.format_prompt(input_text=input_text).to_messages()) 
 
     # Critique or sentiment analysis
     criticism_prompt = ChatPromptTemplate.from_messages([
-        ("system", template),
+        ("system", template3),
         ("human", f"Critique the joke: {refined_output}")
     ])
-    criticized_output = criticism_model(criticism_prompt.format_prompt(input_text=input_text).to_messages())  # Get criticized output
+    criticized_output = criticism_model(criticism_prompt.format_prompt(input_text=input_text).to_messages()) 
 
     # Final output
     final_prompt = ChatPromptTemplate.from_messages([
-        ("system", template),
+        ("system", template4),
         ("human", f"Finalize the joke: {criticized_output}")
     ])
     
-    final_output = finalization_model(final_prompt.format_prompt(input_text=input_text).to_messages())  # Get final output
+    final_output = finalization_model(final_prompt.format_prompt(input_text=input_text).to_messages())
 
     st.info(final_output)
 
