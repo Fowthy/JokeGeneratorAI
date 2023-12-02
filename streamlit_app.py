9515cbf0dd9a82367fb20ca8bc1d9302c8ed857c
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-02 23:57:44 +0100

    Created initial model chain

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from langchain.llms import OpenAI, GPT3, DialoGPT
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+
 
 from langchain.schema import HumanMessage
 from langchain.prompts import PromptTemplate
@@ -18,11 +20,19 @@ joke_type = st.sidebar.text_input('Joke Type')
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     chat_model = ChatOpenAI()
-    joke_prompt = f"Generate a {joke_type.lower()} joke: {input_text}"
-    joke_response = llm(joke_prompt)
 
-    messages = [HumanMessage(content=text)]
-    respomse = chat_model.invoke(messages)
+    template = "You are an AI that generates {joke_type} jokes."
+    human_template = "Generate a joke about {input_text}"
+
+    chat_prompt = ChatPromptTemplate.from_messages([
+    ("system", template),
+    ("human", human_template),
+    ])
+
+    chat_prompt.format_messages(joke_type=joke_type, input_text=input_text)
+
+    chain = chat_prompt | chat_model
+    respomse = chain.invoke()
 
     st.info(respomse)
 
