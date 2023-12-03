280717a814dfd412e3520d62ad1df73d9e87fd58
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-03 11:05:56 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,61 +19,64 @@ age = st.sidebar.text_input('Age')
 def health_advisor(input_text, vector_store):
     general_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    template = "You are an AI health and fitness advisor. You answer with short, concrete answers, not general answers. You generalize the user's input and you create concrete goal for the user. Your prompt will be used in other AI model, so you must write to be understandable."
+    template = "You are an AI health and fitness advisor. You answer with short, concrete answers, not general answers. You generalize the user's input and you create concrete goal for the user. You don't stop asking questions until you have all the information you need to answer with maximum accuracy based on the person's features."
+
+    vector_store.setdefault('user_goal', []).append(f"User's weight in kg: {weight_kg}, User's height in cm: {height_cm}, User's age: {age}, User's goal: {input_text}")
 
     # Construct the chat prompt with vector store information
     chat_prompt = ChatPromptTemplate.from_messages([
         template,
-        f"User's weight in kg: {weight_kg}, User's height in cm: {height_cm}, User's age: {age}, User's goal: {input_text}"
+        vector_store.get('user_goal')
     ])
 
     # Invoke the model chain
     chain = chat_prompt | general_model | ParseOutput()
     response = chain.invoke(vector_store)
 
+    st.info(response)
 
-    nutritional_advice_model = ChatOpenAI(openai_api_key=openai_api_key)
+    # nutritional_advice_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    nutritional_model_template = "You are an AI nutritionist. Based on the user's goal, provide specific nutritional advice of what he should eat and what he should not eat, but no recipes. Only information about protein, vitamins, carbs, fat, etc."
-    nutri_chat_prompt = ChatPromptTemplate.from_messages([
-        nutritional_model_template,
-        f"{response}"
-    ])
+    # nutritional_model_template = "You are an AI nutritionist. Based on the user's goal, provide specific nutritional advice of what he should eat and what he should not eat, but no recipes. Only information about protein, vitamins, carbs, fat, etc."
+    # nutri_chat_prompt = ChatPromptTemplate.from_messages([
+    #     nutritional_model_template,
+    #     f"{response}"
+    # ])
 
-    nutri_chain = nutri_chat_prompt | nutritional_advice_model | ParseOutput()
-    nutri_response = nutri_chain.invoke(vector_store)
+    # nutri_chain = nutri_chat_prompt | nutritional_advice_model | ParseOutput()
+    # nutri_response = nutri_chain.invoke(vector_store)
 
-    recipe_model = ChatOpenAI(openai_api_key=openai_api_key)
+    # recipe_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    recipe_template = "You are an AI chef. Based on the user's goal, provide specific recipes of what he should eat and what he should not eat. You must use the nutritional advice from the previous model. You must write short and concrete list with quick foods that the user can eat to achieve his goal, nothing else. Only the list."
-    recipe_chat_prompt = ChatPromptTemplate.from_messages([
-        recipe_template,
-        f"Nutritional AI model response: {nutri_response}"
-    ])
-    recipe_chain = recipe_chat_prompt | recipe_model | ParseOutput()
-    recipe_response = recipe_chain.invoke(vector_store)
+    # recipe_template = "You are an AI chef. Based on the user's goal, provide specific recipes of what he should eat and what he should not eat. You must use the nutritional advice from the previous model. You must write short and concrete list with quick foods that the user can eat to achieve his goal, nothing else. Only the list."
+    # recipe_chat_prompt = ChatPromptTemplate.from_messages([
+    #     recipe_template,
+    #     f"Nutritional AI model response: {nutri_response}"
+    # ])
+    # recipe_chain = recipe_chat_prompt | recipe_model | ParseOutput()
+    # recipe_response = recipe_chain.invoke(vector_store)
 
-    combined = response + nutri_response + recipe_response
+    # combined = response + nutri_response + recipe_response
 
-    conclude_model = ChatOpenAI(openai_api_key=openai_api_key)
+    # conclude_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    conclude_template = "You are an AI health and fitness advisor. You answer with short, concrete answers, not general answers. You must take the response from the previous models and give concrete advice to the user with 2-3 sentences."
+    # conclude_template = "You are an AI health and fitness advisor. You answer with short, concrete answers, not general answers. You must take the response from the previous models and give concrete advice to the user with 2-3 sentences."
 
-    # Construct the chat prompt with vector store information
-    conclude_prompt = ChatPromptTemplate.from_messages([
-        f"{conclude_template} Model response: {combined}",
-        f"User's input: {input_text}"
-    ])
-
-
-    conclude_chain = conclude_prompt | conclude_model | ParseOutput()
-    conclude_response = conclude_chain.invoke(vector_store)
+    # # Construct the chat prompt with vector store information
+    # conclude_prompt = ChatPromptTemplate.from_messages([
+    #     f"{conclude_template} Model response: {combined}",
+    #     f"User's input: {input_text}"
+    # ])
 
 
-    st.session_state.meal_vector_store = vector_store
-    st.info(conclude_response)
-    st.info(nutri_response)
-    st.info(recipe_response)
+    # conclude_chain = conclude_prompt | conclude_model | ParseOutput()
+    # conclude_response = conclude_chain.invoke(vector_store)
+
+
+    # st.session_state.meal_vector_store = vector_store
+    # st.info(conclude_response)
+    # st.info(nutri_response)
+    # st.info(recipe_response)
 
 with st.form('fitness_form'):
     text = st.text_area('Enter your body goal.')
