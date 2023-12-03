35f934decd6da18d90e841c7f4241632c1868afd
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-03 11:40:41 +0100

    Added history to mood analyzer

--- mood_analyzer_app.py
+++ mood_analyzer_app.py
@@ -8,13 +8,16 @@ prompt_model = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 mood_model = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
 
 def generate_response_and_analyze_mood(input_text):
+    
     # Model for generating responses
-    response_prompt = f"Generate a response to: {input_text}"
-    response = prompt_model(response_prompt)
+    vector_store.setdefault('moodanalyzer_history', []).append(f"User's input: {input_text}")
+    response = prompt_model(vector_store.get('moodanalyzer_history'))
 
     # Model for analyzing mood
     mood_prompt = f"Analyze the mood of: {input_text}. Use only one of three colorful emojis to describe the mood. Green, yellow or red., where green is friendly, yellow is neutral and red is angry. You output only the emoji, no quotes or other text. You output text only when the red is angry and there is something really wrong that must be pointed out."
     mood_analysis = mood_model(mood_prompt)
+
+
 
     st.info(response)
 
@@ -23,9 +26,12 @@ def generate_response_and_analyze_mood(input_text):
 with st.form('my_form'):
     text = st.text_area('Enter text:')
     submitted = st.form_submit_button('Submit')
+    vector_store = st.session_state.get('moodanalyzer_vector_store', {})
+
 
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
 
     if submitted and openai_api_key.startswith('sk-'):
+        st.session_state.health_vector_store = vector_store
         generate_response_and_analyze_mood(text)
