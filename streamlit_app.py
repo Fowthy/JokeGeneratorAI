f54d5f94f8194abfc7219eb0f5b12bca6b0671df
Alex Svetoslavov <451875@student.fontys.nl>
2023-12-03 01:45:04 +0100

    Added message history

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,47 +7,55 @@ from typing import List
 
 class ParseOutput(BaseOutputParser[List[str]]):
     def parse(self, text: str) -> List[str]:
-        return text.strip().split('\n')
+        return text
 
-st.title('Meal Planner and Tracker')
+st.title('Health and Fitness Advisor')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-def meal_planner(input_text, vector_store):
+def health_advisor(input_text, vector_store):
     chat_model = ChatOpenAI(openai_api_key=openai_api_key)
 
-    template = "You are an AI meal planner and tracker."
+    template = "You are an AI health and fitness advisor. You answer with short, concrete answers. You provide health and fitness advice based on the user's goals, progress, and preferences."
 
+    vector_store.setdefault('message_history', []).append(f"User's input: {input_text}")
+
+    # Construct the chat prompt with vector store information
     chat_prompt = ChatPromptTemplate.from_messages([
         HumanMessage(content=template),
-        HumanMessage(content=f"User's input: {input_text}")
+        *vector_store.get('message_history', [])
     ])
 
-    # Access vector store information (history)
-    if 'meal_plan' in vector_store:
-        chat_prompt.add_message(HumanMessage(content=f"User's meal plan: {vector_store['meal_plan']}"))
-    if 'foods_eaten' in vector_store:
-        chat_prompt.add_message(HumanMessage(content=f"Foods eaten: {vector_store['foods_eaten']}"))
-    if 'remaining_foods' in vector_store:
-        chat_prompt.add_message(HumanMessage(content=f"Remaining foods: {vector_store['remaining_foods']}"))
+    # Access vector store information
+    if 'user_goals' in vector_store:
+        chat_prompt.add_message(HumanMessage(content=f"User's goals: {vector_store['user_goals']}"))
+    if 'user_progress' in vector_store:
+        chat_prompt.add_message(HumanMessage(content=f"User's progress: {vector_store['user_progress']}"))
+    if 'user_preferences' in vector_store:
+        chat_prompt.add_message(HumanMessage(content=f"User's preferences: {vector_store['user_preferences']}"))
 
+    # Update vector store with new information
     vector_store['input_text'] = input_text
 
+    # Invoke the model chain
     chain = chat_prompt | chat_model | ParseOutput()
     response = chain.invoke(vector_store)
 
+    vector_store.setdefault('message_history', []).append(f"Model's response: {response}")
+    
     st.session_state.meal_vector_store = vector_store
-
     st.info(response)
 
-with st.form('meal_form'):
-    text = st.text_area('Enter your meal-related query.')
+with st.form('fitness_form'):
+    text = st.text_area('Enter your health and fitness query.')
     submitted = st.form_submit_button('Submit')
 
-    # Initialize or retrieve vector store (history)
-    vector_store = st.session_state.get('meal_vector_store', {})
+    # Initialize or retrieve vector store
+    vector_store = st.session_state.get('health_vector_store', {})
 
     if submitted and openai_api_key.startswith('sk-'):
-        meal_planner(text, vector_store)
+        health_advisor(text, vector_store)
+        # Update the vector store for future conversations
+        st.session_state.health_vector_store = vector_store
     elif not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
